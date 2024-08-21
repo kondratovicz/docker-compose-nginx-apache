31cc9f92b7a065b3d65636695f12cbcdc207670e
oracle <ubunta@oracle.cloud>
2024-08-21 16:21:05 +0000

    Just started

---
+++ apache/htdocs/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>it works yo</title>>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <h1>Welcome to My Website</h1>
+    <p>This is the test homepage.</p>
+    <p><a href="info.php">PHP Info</a></p>
+    <p><a href="contact.php">Contact</a></p>
+</body>
+</html>
+
