26f92d621b574bfc7bdd29e3f3902decf019adc2
Pavel Kondratovich <kondratovich@ro.ru>
2024-08-26 16:04:12 +0300

    fix

--- apache/htdocs/date_image.php
+++ apache/htdocs/date_image.php
@@ -1,14 +1,6 @@
 <?php
-header('Content-Type: image/jpg');
-$image = imagecreatefromjpeg('/var/www/static/images/clock.jpg');
+echo "<h1>Current Time: " . date("Y-m-d H:i:s") . "</h1>";
 
-$color = imagecolorallocate($image, 255, 255, 255);
-$font = 5;
-$date = date('Y-m-d H:i:s');
-$x = 10;
-$y = 10;
-
-imagestring($image, $font, $x, $y, $date, $color);
-imagejpeg($image);
-imagedestroy($image);
+$image_url = '/images/clock.jpg';
+echo "<img src='$image_url' alt='Image from NGINX' />";
 ?>
